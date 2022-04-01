d723d42b81e1a48173fb6d249fe968a592aa6d86
AlexanderShtarev <aeresfiru@gmail.com>
2022-04-01 23:37:04 +0300

    Add validation for entities

--- core/src/main/java/com/epam/esm/domain/Certificate.java
+++ core/src/main/java/com/epam/esm/domain/Certificate.java
@@ -1,9 +1,11 @@
 package com.epam.esm.domain;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -14,12 +16,31 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Certificate {
+
     private Long id;
+
+    @NotEmpty(message = "Name may not be empty")
+    @Length(min = 3, max = 100, message = "Name should be between 3 and 100 characters long")
     private String name;
+
+    @NotEmpty(message = "Description may not be empty")
+    @Length(min = 20, max = 500, message = "Description should be between 20 and 500 characters long")
     private String description;
+
+    @DecimalMin(value = "0.0", inclusive = false)
+    @Digits(integer = 3, fraction = 2)
     private BigDecimal price;
+
+    @Min(value = 3, message = "Duration should be more than 3 days")
+    @Max(value = 365, message = "Duration should be lesser than year")
     private Short duration;
+
+    @NotNull
     private LocalDateTime createDate;
+
+    @NotNull
     private LocalDateTime lastUpdateDate;
+
+    @NotEmpty
     private Set<Tag> tags;
 }
--- core/src/main/java/com/epam/esm/domain/Tag.java
+++ core/src/main/java/com/epam/esm/domain/Tag.java
@@ -1,9 +1,11 @@
 package com.epam.esm.domain;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @AllArgsConstructor
@@ -11,5 +13,8 @@ import lombok.NoArgsConstructor;
 @Builder
 public class Tag {
     private Long id;
+
+    @NotEmpty(message = "Name may not be empty")
+    @Length(min = 3, max = 100, message = "Name should be between 3 and 100 characters long")
     private String name;
 }
